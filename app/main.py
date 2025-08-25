29037f5dd011651c3b2a4dc8ecc431eb04db4770
Andreas Chatzopoulos <andreas.chatzopoulos@gmail.com>
2025-08-25 22:28:08 +0200

    ...

--- app/main.py
+++ app/main.py
@@ -26,7 +26,6 @@ def latest_obs_on_file() -> str:
     latest_file = max(list_of_files, key=os.path.getctime)
     return latest_file
 
-
 # Upload OBS to LLM
 async def obs_upload():
     try:
@@ -44,9 +43,6 @@ async def obs_upload():
     return result
 
 app = FastAPI()
-'''
-we
-'''
 
 # Test
 @app.get("/", response_model=Actions)
