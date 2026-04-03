040ac853a9c0345c091df2513cafb0e0cffa7797
Andreas Chatzopoulos <andreas.chatzopoulos@gmail.com>
2026-04-03 21:21:15 +0200

    Added db connection

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from fastapi import FastAPI, UploadFile, HTTPException, Depends, status
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
 import glob
 import os
 import time
 
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    user_name: str = Field(index=True)
+    password: str
+
+class CogGraph(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    model_name: str = Field(index=True)
+    graph_json: str
+    owner_id: int = Field(foreign_key="user.id")
+
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+
+def create_db_and_tables():
+    # SQLModel.metadata.drop_all(engine)
+    SQLModel.metadata.create_all(engine)
+    
+
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 # Save OBS to disk
 def save_obs_to_disk(file: UploadFile) -> str:
